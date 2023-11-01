7d22b99c094fd384390c1ce3839f5bd9aa00c003
lucasops96 <lucasbanana96@gmail.com>
2023-11-01 11:52:02 -0300

    serializer relations in recipes

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,12 +1,34 @@
 # flake8: noqa
 from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class CategorySerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=65)
+
+class TagSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=255)
+    slug = serializers.SlugField()
 
 class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=65)
     description = serializers.CharField(max_length=165)
     public = serializers.BooleanField(source='is_published')
-    preparation = serializers.SerializerMethodField()
+    preparation = serializers.SerializerMethodField(method_name='any_method_name')
 
-    def get_preparation(self,obj):
+    category = serializers.StringRelatedField()
+    # category = CategorySerializer(many=False)
+    # category = serializers.PrimaryKeyRelatedField(
+    #     queryset=Category.objects.all(),
+    # )
+
+    author = serializers.PrimaryKeyRelatedField(
+        queryset= User.objects.all()
+    )
+
+    tags = TagSerializer(many=True)
+    # tags = serializers.StringRelatedField(many=True)
+
+    def any_method_name(self,obj):
         return f'{obj.preparation_time} {obj.preparation_time_unit}'
