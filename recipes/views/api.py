209219c941001b84ed390d85a627076136600da7
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2024-01-11 16:03:11 -0300

    implementando metodo create

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -68,6 +68,13 @@ class RecipeAPIv2ViewSet(ModelViewSet):
         print(request.user.is_authenticated)
 
         return super().list(request, *args, **kwargs)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def partial_update(self, request, *args, **kwargs):
         recipe = self.get_object()
