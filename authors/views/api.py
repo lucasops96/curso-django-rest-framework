ce1a6578a58744231754760963ecf6ecf694093a
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2024-01-15 11:40:00 -0300

    create endpoint me and post with multipart form data

--- authors/views/api.py
+++ authors/views/api.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from ..serializers import AuthorSerializer
 
 class AuthorViewSet(ReadOnlyModelViewSet):
@@ -12,3 +14,10 @@ class AuthorViewSet(ReadOnlyModelViewSet):
         qs = User.objects.filter(id=self.request.user.id)
         return qs
     
+    @action(methods=['get'],detail=False,)
+    def me(self, request, *args, **kwargs):
+        obj = self.get_queryset().first()
+        serializer = self.get_serializer(
+            instance = obj
+        )
+        return Response(serializer.data)
